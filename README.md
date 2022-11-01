d4e10a0d795fb74739dc965e07de4fedcb4a3226
Jonathan Beckett <jonathan.beckett@gmail.com>
2022-11-01 09:49:30 +0000

    First commit!

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # csv2obsidian
-Exports the contents of a CSV into a collection of Obsidian formatted files
+
+I wrote this script largely for my own purposes, but thought the community might find use for it too. The script exports any CSV file into a collection of markdown files formatted for use in Obsidian - complete with YAML for each field value, for those using the dataview plugin.
+
+Note that the files are named after the first column in the CSV. Also, where values have multiple lines, only the first line is picked up.
---
+++ csv2obsidian.py
@@ -0,0 +1,42 @@
+import csv
+import sys
+import os
+
+# Converts a CSV file to a collection of markdown files
+
+if (len(sys.argv)!=3):
+    print("\ncsv2obsidian.py - by Jonathan Beckett\n\npython csv2obsidian.py <source_file> <output_directory>\n\nExample : python csv2obsidian.py file.csv c:\\my\\vault\\subfolder\n\n")
+    sys.exit(0)
+
+source_file = sys.argv[1]
+output_path = sys.argv[2]
+
+with open(source_file,newline="") as csvfile:
+    csvreader = csv.DictReader(csvfile,delimiter=",",quotechar="\"")
+    for row in csvreader:
+
+        print("Processing " + row[csvreader.fieldnames[0]])
+
+        # build the yaml
+        output_text = "---\n"
+        for fieldname in csvreader.fieldnames:
+            if (row[fieldname]):
+                output_text += fieldname.replace(" ","").lower() + ": " + "".join(row[fieldname].split("\n")[0]) + "\n"
+        output_text += "---\n"
+
+        # build the human readable version
+        
+        for fieldname in csvreader.fieldnames:
+            if(row[fieldname]):
+                output_text += "\n### " + fieldname + "\n"
+                output_text += row[fieldname] + "\n"
+
+        # create an output file name from the first column
+        file_path = os.path.join(output_path,row[csvreader.fieldnames[0]]) + ".md"       
+        print(" > " + file_path)
+        
+        # output to a file        
+        output_file_path = file_path
+        output_file = open(output_file_path, 'wb')
+        output_file.write(bytes(output_text,'utf-8'))
+        output_file.close()
